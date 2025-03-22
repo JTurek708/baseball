3ab428f0607d0c4c9e3a1d069c7632fee282d791
Jack Turek <jackturek@jacks-mbp.lan>
2025-03-21 21:57:39 -0500

    Adding os key

--- statcast pipeline/season_2025.py
+++ statcast pipeline/season_2025.py
@@ -3,6 +3,10 @@ import pandas as pd
 from datetime import datetime, timedelta
 from pybaseball import statcast
 from google.cloud import storage
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/jackturek/Documents/Repos/baseball/statcast pipeline/baseball-450617-d3588fea7841.json"
+
 
 def run_pipeline_for_yesterday(bucket_name: str, master_filename: str = "master2025.csv"):
     # 1. Calculate yesterday's date
